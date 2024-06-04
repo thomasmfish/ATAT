f2ebdc8a375f61cbb9322dcd903a4532b7dc3031
Thomas M Fish <thomasmfish@gmail.com>
2024-06-04 19:31:54 +0100

    `create_dataframe_with_duplicate`: ensure it returns the correct values

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pandas as pd
-from copy import deepcopy
+import numpy as np
 
 __example_data_dir = Path(__file__).parent / "example_data"
 __input_data_filename = "input_data.csv"
@@ -27,26 +27,27 @@ def get_output_plot_path() -> Path:
 def create_dataframe_with_duplicate(
     same_optical_density: bool, same_amino_acid_sequence: bool
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
-    duplicated_row = 2
+    row_index = 2
     columns = ["index", "optical_density", "amino_acid_sequence"]
-    test_data = [
-        ["A01", 0.8, "GFTFSSYF"],
-        ["A02", 1.3, "GFTFSNYA"],
-        ["A03", 1.3, "GFTFSSYW"],
-        ["A04", 0.9, "GFPFEMYD"],
-        ["A05", 0.4, "GFTFDDYA"],
-    ]
-    duplicate_test_data = deepcopy(test_data)
-    duplicate_row = duplicate_test_data[duplicated_row][:]
+    test_data = np.asarray(
+        [
+            ["A02", 1.3, "GFTFSNYA"],
+            ["A03", 1.3, "GFTFSSYW"],
+            ["A04", 0.9, "GFPFEMYD"],
+            ["A05", 0.4, "GFTFDDYA"],
+        ],
+        dtype=np.object_,
+    )
 
+    duplicate_row = test_data[row_index, :].copy()
     if not same_optical_density:
         duplicate_row[1] = duplicate_row[1] + 0.2
 
     if not same_amino_acid_sequence:
-        duplicate_row[2] = "ABCDEFGH"
+        duplicate_row[2] = "ABCDEFGH"  #
 
-    duplicate_test_data.append(duplicate_row)
+    duplicate_test_data = np.vstack((test_data, duplicate_row))
 
     return pd.DataFrame(duplicate_test_data, columns=columns), pd.DataFrame(
-        test_data, columns=columns
+        np.asarray(test_data)[:, 1:], columns=columns[1:]
     )
