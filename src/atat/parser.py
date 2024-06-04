7346c7e3fe2d9730859c258cb192dbc10f10ac17
Thomas M Fish <thomasmfish@gmail.com>
2024-06-04 21:00:03 +0100

    Reduce to minimum required, add `image_type` choices and default

--- src/atat/parser.py
+++ src/atat/parser.py
@@ -1,16 +1,22 @@
+from __future__ import annotations
 import argparse
-from typing import Any
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 
-def parse_args(*args: str) -> dict[str:Any]:
+def parse_args(*args: str, supported_image_types: Iterable[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog="ATAT")
-    parser.add_argument("input_file", nargs="?")
-    parser.add_argument("-i", "--input", dest="input_file")
-    parser.add_argument("output_directory", nargs="?")
+    parser.add_argument("-i", "--input", dest="input_file", required=True)
     parser.add_argument("-o", "--output-directory", dest="output_directory")
     parser.add_argument("-n", "--output-name")
-    parser.add_argument("-dp", "--decimals", dest="decimal_places")
-    parser.add_argument("-im", "--image-type")
-    parser.add_argument("--version", action="store_true")
+    parser.add_argument("-dp", "--decimals", dest="decimal_places", type=int)
+    parser.add_argument(
+        "-im",
+        "--image-type",
+        choices=supported_image_types,
+        default=supported_image_types[0],
+    )
 
-    return vars(parser.parse_args(args))
+    return parser.parse_args(args)
