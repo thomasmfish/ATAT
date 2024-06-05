79da5052843ee9cc72b9a88a617345939dab4a63
Thomas M Fish <thomasmfish@gmail.com>
2024-06-05 04:07:44 +0100

    get io working

--- src/atat/file_handling.py
+++ src/atat/file_handling.py
@@ -6,6 +6,16 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from os import PathLike
+    from matplotlib.figure import Figure
 
 
-def read_input(file_path: str | PathLike[str]) -> pd.DataFrame: ...
+def read_input(file_path: str | PathLike[str]) -> pd.DataFrame:
+    return pd.read_csv(file_path)
+
+
+def save_dataframe(file_path: str | PathLike[str], dataframe: pd.DataFrame) -> None:
+    dataframe.to_csv(file_path, index=False, lineterminator="\n")
+
+
+def save_figure(file_path: str | PathLike[str], figure: Figure) -> None:
+    figure.savefig(file_path)
